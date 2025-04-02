89a9a9c9ac61056283bff35faca1d8fe08ac7f08
Nathan Wibowo <nathan.wibowo@teambespin.us>
2025-04-02 09:14:01 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/awks/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.awks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key,});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(onPressed: (){}, icon: const Icon(Icons.schedule), color: Colors.white,),
+        actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.settings),color: Colors.white,)],
+        title: Text('AWKS',style: GoogleFonts.righteous(color: Colors.white, fontSize: 36)),
+        scrolledUnderElevation: 0,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment(0.8, 1),
+            colors: <Color>[
+              Color(0xFF21143B),
+              Color(0xFF5F00B5),
+            ], 
+            tileMode: TileMode.mirror,
+          ),
+        ),
+        child: ListView(
+          children: const <Widget>[
+            ListTile(leading: Icon(Icons.map), title: Text('Map')),
+            ListTile(leading: Icon(Icons.photo_album), title: Text('Album')),
+            ListTile(leading: Icon(Icons.phone), title: Text('Phone')),
+          ],
+        ),
+      ),
+    );
+  }
+}
